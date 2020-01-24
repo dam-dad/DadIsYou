ffa28a78f91c896ca899762518eba942cd133ff7
Cristian Trujillo <cristiantllotllo2424@gmail.com>
2020-01-24 10:37:33 +0000

    commit

---
+++ src/main/java/dad/App.java
@@ -0,0 +1,19 @@
+package dad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/dad/Main.java
@@ -0,0 +1,9 @@
+package dad;
+
+public class Main {
+
+	public static void main(String[] args) {
+		App.main(args);
+	}
+
+}
