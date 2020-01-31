6aead7aa300b77ccaa6eae5d66d55f4b73f55cb6
Beatriz Garcia <beagh92@gmail.com>
2020-01-31 12:08:43 +0000

    primer nivel añadido

--- src/main/java/dad/game/model/Nivel.java
+++ src/main/java/dad/game/model/Nivel.java
@@ -1,7 +1,5 @@
 package dad.game.model;
 
-import dad.App;
-
 public class Nivel {
 	
 	public static Objeto[][] uno() {
@@ -10,11 +8,11 @@ public class Nivel {
 				"000000000000000000000000",
 				"0000000000diy00000000000",
 				"000000000000000000000000",
-				"0000MMMMMMMMMMMMMMMM0000",
+				"0000WWWWWWWWWWWWWWWW0000",
 				"000000000000000000000000",
 				"00000D000000000000F00000",
 				"000000000000000000000000",
-				"00000MMMMMMMMMMMMMMM0000",
+				"00000WWWWWWWWWWWWWWW0000",
 				"000000000000000000000000",
 				"0000000000fiw00000000000",
 				"000000000000000000000000",
@@ -25,8 +23,46 @@ public class Nivel {
 		return parsear(nivel);
 	}
 	
+	/**
+	 * 
+	 * @param nivel
+	 * @return
+	 */
 	private static Objeto[][] parsear(String[] nivel) {
-		return null;
+		Objeto[][] mapa = new Objeto[nivel.length][nivel[0].length()];
+		for (int i = 0; i < nivel.length; i++) {
+			for (int j = 0; j < nivel[i].length(); j++) {
+				char letra = nivel[i].charAt(j);
+				Posicion posicion = new Posicion(i, j);
+				if(letra == '0') { // Aire
+					mapa[i][j] = null;
+				} else if (letra == 'W') {
+					mapa[i][j] = new Elemento(SujetoEnum.WALL); 
+				} else if (letra == 'F') {
+					mapa[i][j] = new Elemento(SujetoEnum.FLAG);
+				} else if (letra == 'D') {
+					mapa[i][j] = new Elemento(SujetoEnum.DAD);
+				} else if (letra == 'd') {
+					mapa[i][j] = new Sujeto(SujetoEnum.DAD);
+				} else if (letra == 'i') {
+					mapa[i][j] = new Verbo(VerboEnum.IS);
+				} else if (letra == 'y') {
+					mapa[i][j] = new Accion(AccionEnum.YOU);
+				} else if (letra == 'f') {
+					mapa[i][j] = new Sujeto(SujetoEnum.FLAG);
+				} else if (letra == 'w') {
+					mapa[i][j] = new Accion(AccionEnum.WIN);
+				}
+				
+				//Añadir posición cuando no sea aire
+				if(mapa[i][j] != null) {
+					mapa[i][j].setPosicion(posicion);
+				}
+				
+			}
+		}
+		
+		return mapa;
 	}
 	
 	
