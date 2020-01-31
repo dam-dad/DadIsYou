ccf68046e812a538c835667b1a490d02e86cdd54
Beatriz Garcia <beagh92@gmail.com>
2020-01-31 10:21:26 +0000

    modelos añadidos

---
+++ src/main/java/dad/game/model/Posicion.java
@@ -0,0 +1,55 @@
+package dad.game.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Posicion {
+	private IntegerProperty x = new SimpleIntegerProperty();
+	private  IntegerProperty y = new SimpleIntegerProperty();
+	
+	public Posicion() {}
+
+	public Posicion(int x, int y) {
+		super();
+		this.x.set(x);
+		this.y.set(y);
+	}
+
+	public IntegerProperty xProperty() {
+		return this.x;
+	}
+	
+
+	public int getX() {
+		return this.xProperty().get();
+	}
+	
+
+	public void setX(final int x) {
+		this.xProperty().set(x);
+	}
+	
+
+	public IntegerProperty yProperty() {
+		return this.y;
+	}
+	
+
+	public int getY() {
+		return this.yProperty().get();
+	}
+	
+
+	public void setY(final int y) {
+		this.yProperty().set(y);
+	}
+	
+
+
+
+	
+
+	
+	
+	
+}
