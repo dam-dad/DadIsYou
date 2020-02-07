cb09db6bb07ed18981789822fde3b61e8a2b9002
Cristian Trujillo <cristiantllotllo2424@gmail.com>
2020-02-07 11:16:48 +0000

    Objetos en segundo plano

--- src/main/java/dad/game/model/Tablero.java
+++ src/main/java/dad/game/model/Tablero.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Tablero {
 	private int cantidadColumnas;
 	private int cantidadFilas;
-	private Objeto<?>[][] posicionObjetos;
+	private Objeto<?>[][] objetos;
 	private ArrayList<Objeto<?>[][]> historial;
 	private ArrayList<Objeto<?>[]> frases;
+	private ArrayList<Objeto<?>> objetosSegundoPlano;
 
 	public Tablero() {
 	}
@@ -15,9 +16,10 @@ public class Tablero {
 	public Tablero(int cantidadColumnas, int cantidadFilas) {
 		this.cantidadColumnas = cantidadColumnas;
 		this.cantidadFilas = cantidadFilas;
-		this.posicionObjetos = new Objeto[cantidadColumnas][cantidadFilas];
+		this.objetos = new Objeto[cantidadColumnas][cantidadFilas];
 		this.historial = new ArrayList<Objeto<?>[][]>();
 		this.frases = new ArrayList<Objeto<?>[]>();
+		this.objetosSegundoPlano = new ArrayList<Objeto<?>>();
 	}
 
 	public int getCantidadColumnas() {
@@ -37,15 +39,15 @@ public class Tablero {
 	}
 
 	public Objeto<?>[][] getPosicionObjetos() {
-		return posicionObjetos;
+		return objetos;
 	}
 
 	public void setPosicionObjetos(Objeto<?>[][] posicionObjetos) {
-		this.posicionObjetos = posicionObjetos;
+		this.objetos = posicionObjetos;
 	}
 
 	public void cargarNivel(Objeto<?>[][] nivel) {
-		this.posicionObjetos = nivel;
+		this.objetos = nivel;
 		comprobarFrases();
 		asignarEstados();
 	}
@@ -53,57 +55,47 @@ public class Tablero {
 	public void mover(DireccionEnum direccion) {
 		ArrayList<Objeto<?>> elementosYou = buscarElemento(AccionEnum.YOU);
 
-		if(elementosYou.size() > 0) {
-			ArrayList<Objeto<?>> elementoAnterior = new ArrayList<Objeto<?>>();
+		if (elementosYou.size() > 0) {
+			int x = 0, y = 0;
 			if (direccion == DireccionEnum.ARRIBA) {
-				for(Objeto<?> you : elementosYou) {
-					if(you.getPosicion().getY() - 1 >= 0) {
-						elementoAnterior.add(posicionObjetos[you.getPosicion().getY()-1][you.getPosicion().getX()]);
-						posicionObjetos[you.getPosicion().getY()-1][you.getPosicion().getX()] = posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()];
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()] = null;
-						you.getPosicion().mover(DireccionEnum.ARRIBA, 1);
+				y = -1;
+			} else if (direccion == DireccionEnum.ABAJO) {
+				y = 1;
+			} else if (direccion == DireccionEnum.DERECHA) {
+				x = 1;
+			} else if (direccion == DireccionEnum.IZQUIERDA) {
+				x = -1;
+			}
+			for (Objeto<?> you : elementosYou) {
+				if (you.getPosicion().getY() + y >= 0 && you.getPosicion().getY() + y < cantidadFilas
+						&& you.getPosicion().getX() + x >= 0 && you.getPosicion().getX() + x < cantidadColumnas) {
+					if (objetos[you.getPosicion().getY() + y][you.getPosicion().getX() + x] != null) {
+						objetosSegundoPlano.add(objetos[you.getPosicion().getY() + y][you.getPosicion().getX() + x]);
 					}
-				}
-			} else if(direccion == DireccionEnum.ABAJO) {
-				for(Objeto<?> you : elementosYou) {
-					if(you.getPosicion().getY() + 1 <= cantidadFilas - 1) {
-						elementoAnterior.add(posicionObjetos[you.getPosicion().getY()+1][you.getPosicion().getX()]);
-						posicionObjetos[you.getPosicion().getY()+1][you.getPosicion().getX()] = posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()];
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()] = null;
-						you.getPosicion().mover(DireccionEnum.ABAJO, 1);
+					objetos[you.getPosicion().getY() + y][you.getPosicion().getX()
+							+ x] = objetos[you.getPosicion().getY()][you.getPosicion().getX()];
+					objetos[you.getPosicion().getY()][you.getPosicion().getX()] = null;
+					for (Objeto<?> objetoSegundoPlano : objetosSegundoPlano) {
+						if (objetoSegundoPlano.getPosicion().getX() == you.getPosicion().getX()
+								&& objetoSegundoPlano.getPosicion().getY() == you.getPosicion().getY()) {
+							objetos[you.getPosicion().getY()][you.getPosicion().getX()] = objetoSegundoPlano;
+						}
 					}
-				}
-			} else if(direccion == DireccionEnum.DERECHA) {
-				for(Objeto<?> you : elementosYou) {
-					if(you.getPosicion().getX() + 1 <= cantidadColumnas - 1) {
-						elementoAnterior.add(posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()+1]);
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()+1] = posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()];
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()] = null;
-						you.getPosicion().mover(DireccionEnum.DERECHA, 1);
-					}
-					
-				}
-			} else if(direccion == DireccionEnum.IZQUIERDA) {
-				for(Objeto<?> you : elementosYou) {
-					if(you.getPosicion().getX() - 1 >= 0) {
-						elementoAnterior.add(posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()-1]);
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()-1] = posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()];
-						posicionObjetos[you.getPosicion().getY()][you.getPosicion().getX()] = null;
-						you.getPosicion().mover(DireccionEnum.IZQUIERDA, 1);
-					}
+					objetosSegundoPlano.remove(objetos[you.getPosicion().getY()][you.getPosicion().getX()]);
+					you.getPosicion().mover(direccion, 1);
 				}
 			}
-			
-			//mostrarTablero();
-			comprobarWin(elementoAnterior);
+
+			// mostrarTablero();
+			comprobarWin();
 			comprobarFrases();
-			//mostrarFrases();
+			// mostrarFrases();
 			asignarEstados();
-			//mostrarEstados();
+			// mostrarEstados();
 			comprobarDefeat();
 		}
 	}
-	
+
 	private void comprobarDefeat() {
 		ArrayList<Objeto<?>> elementosYou = buscarElemento(AccionEnum.YOU);
 		if (elementosYou.size() == 0) {
@@ -111,27 +103,24 @@ public class Tablero {
 		}
 	}
 
-	private void comprobarWin(ArrayList<Objeto<?>> elementoAnterior ) {
-		for (Objeto<?> elemento : elementoAnterior) {
-			if (elemento != null) {
-				for (int i = 0; i < elemento.getEstados().size(); i++) {
-					if(elemento.getEstados().get(i) == AccionEnum.WIN) {
-						System.out.println("¡¡¡¡¡ HAS GANADO !!!!!");
-					}
+	private void comprobarWin() {
+		for (Objeto<?> elemento : objetosSegundoPlano) {
+			for (int i = 0; i < elemento.getEstados().size(); i++) {
+				if (elemento.getEstados().get(i) == AccionEnum.WIN) {
+					System.out.println("¡¡¡¡¡ HAS GANADO !!!!!");
 				}
 			}
 		}
 	}
 
-	private ArrayList<Objeto<?>> buscarElemento(Object tipo){
+	private ArrayList<Objeto<?>> buscarElemento(Object tipo) {
 		ArrayList<Objeto<?>> elementos = new ArrayList<Objeto<?>>();
-		for (int i = 0; i < posicionObjetos.length; i++) {
-			for (int j = 0; j < posicionObjetos[i].length; j++) {
-				if(posicionObjetos[i][j] != null && 
-						posicionObjetos[i][j].getTipo() == TipoEnum.ELEMENTO) {
-					for(int x = 0; x < posicionObjetos[i][j].getEstados().size(); x++ ) {
-						if( posicionObjetos[i][j].getEstados().get(x) == tipo) {
-							elementos.add(posicionObjetos[i][j]);
+		for (int i = 0; i < objetos.length; i++) {
+			for (int j = 0; j < objetos[i].length; j++) {
+				if (objetos[i][j] != null && objetos[i][j].getTipo() == TipoEnum.ELEMENTO) {
+					for (int x = 0; x < objetos[i][j].getEstados().size(); x++) {
+						if (objetos[i][j].getEstados().get(x) == tipo) {
+							elementos.add(objetos[i][j]);
 						}
 					}
 				}
@@ -142,31 +131,25 @@ public class Tablero {
 
 	private void comprobarFrases() {
 		frases.clear();
-		for (int i = 0; i < posicionObjetos.length; i++) {
-			for (int j = 0; j < posicionObjetos[i].length; j++) {
-				if (posicionObjetos[i][j] != null && posicionObjetos[i][j].getTipo() == TipoEnum.SUJETO) {
+		for (int i = 0; i < objetos.length; i++) {
+			for (int j = 0; j < objetos[i].length; j++) {
+				if (objetos[i][j] != null && objetos[i][j].getTipo() == TipoEnum.SUJETO) {
 					// Comprobar hacia la derecha si se forma una frase
 					if (j + 2 <= cantidadColumnas) { // Comprobar si la posible frase está dentro del escenario
-						if (posicionObjetos[i][j + 1] != null
-								&& posicionObjetos[i][j + 1].getTipo() == TipoEnum.VERBO) {
-							if (posicionObjetos[i][j + 2] != null
-									&& (posicionObjetos[i][j + 2].getTipo() == TipoEnum.ACCION
-											|| posicionObjetos[i][j + 2].getTipo() == TipoEnum.SUJETO)) {
-								Objeto<?>[] frase = { posicionObjetos[i][j], posicionObjetos[i][j + 1],
-										posicionObjetos[i][j + 2] };
+						if (objetos[i][j + 1] != null && objetos[i][j + 1].getTipo() == TipoEnum.VERBO) {
+							if (objetos[i][j + 2] != null && (objetos[i][j + 2].getTipo() == TipoEnum.ACCION
+									|| objetos[i][j + 2].getTipo() == TipoEnum.SUJETO)) {
+								Objeto<?>[] frase = { objetos[i][j], objetos[i][j + 1], objetos[i][j + 2] };
 								frases.add(frase);
 							}
 						}
 					}
 					// Comprobar hacia abajo si se forma una frase
 					if (i + 2 <= cantidadFilas) { // Comprobar si la posible frase está dentro del escenario
-						if (posicionObjetos[i + 1][j] != null
-								&& posicionObjetos[i + 1][j].getTipo() == TipoEnum.VERBO) {
-							if (posicionObjetos[i + 2][j] != null
-									&& (posicionObjetos[i + 2][j].getTipo() == TipoEnum.ACCION
-											|| posicionObjetos[i + 2][j].getTipo() == TipoEnum.SUJETO)) {
-								Objeto<?>[] frase = { posicionObjetos[i][j], posicionObjetos[i + 1][j],
-										posicionObjetos[i + 2][j] };
+						if (objetos[i + 1][j] != null && objetos[i + 1][j].getTipo() == TipoEnum.VERBO) {
+							if (objetos[i + 2][j] != null && (objetos[i + 2][j].getTipo() == TipoEnum.ACCION
+									|| objetos[i + 2][j].getTipo() == TipoEnum.SUJETO)) {
+								Objeto<?>[] frase = { objetos[i][j], objetos[i + 1][j], objetos[i + 2][j] };
 								frases.add(frase);
 							}
 						}
@@ -178,9 +161,9 @@ public class Tablero {
 
 	private void asignarEstados() {
 		ArrayList<Objeto<?>> elementos = new ArrayList<Objeto<?>>();
-		for (int i = 0; i < posicionObjetos.length; i++) {
-			for (int j = 0; j < posicionObjetos[i].length; j++) {
-				Objeto<?> elemento = posicionObjetos[i][j];
+		for (int i = 0; i < objetos.length; i++) {
+			for (int j = 0; j < objetos[i].length; j++) {
+				Objeto<?> elemento = objetos[i][j];
 				if (elemento != null && elemento.getTipo() == TipoEnum.ELEMENTO) {
 					elemento.limpiarEstados();
 					elementos.add(elemento);
@@ -203,12 +186,12 @@ public class Tablero {
 					+ frases.get(i)[2].getNombre());
 		}
 	}
-	
+
 	private void mostrarEstados() {
 		System.out.println("Estados: ");
-		for (int i = 0; i < posicionObjetos.length; i++) {
-			for (int j = 0; j < posicionObjetos[i].length; j++) {
-				Objeto elemento = posicionObjetos[i][j];
+		for (int i = 0; i < objetos.length; i++) {
+			for (int j = 0; j < objetos[i].length; j++) {
+				Objeto elemento = objetos[i][j];
 				if (elemento != null && elemento.getTipo() == TipoEnum.ELEMENTO) {
 					System.out.println(elemento.getNombre() + " => " + elemento.getEstados());
 				}
@@ -218,10 +201,10 @@ public class Tablero {
 
 	private void mostrarTablero() {
 		System.out.println("Tablero: ");
-		for (int i = 0; i < posicionObjetos.length; i++) {
-			for (int j = 0; j < posicionObjetos[i].length; j++) {
-				if (posicionObjetos[i][j] != null) {
-					System.out.print(posicionObjetos[i][j].toString());
+		for (int i = 0; i < objetos.length; i++) {
+			for (int j = 0; j < objetos[i].length; j++) {
+				if (objetos[i][j] != null) {
+					System.out.print(objetos[i][j].toString());
 				} else {
 					System.out.print("·");
 				}
