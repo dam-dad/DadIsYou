3f6bfd557bb61b1d1e77265285bbbf9352465469
Beatriz Garcia <beagh92@gmail.com>
2020-03-02 13:31:28 +0000

    Prueba JasperRecords

--- src/main/java/dad/game/controller/MenuPrincipalController.java
+++ src/main/java/dad/game/controller/MenuPrincipalController.java
@@ -1,8 +1,12 @@
 package dad.game.controller;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import dad.App;
+import dad.informes.factura.FacturaDataProvider;
+import dad.informes.factura.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +17,13 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  * Controlador del menú principal
@@ -32,16 +43,32 @@ public class MenuPrincipalController implements Initializable, Controller {
 
 	/**
 	 * Se ejecuta la vista y el controlador de Ajustes
+	 * 
 	 * @param event
 	 */
 	@FXML
 	void onAjustesAction(ActionEvent event) {
 		App.getScreenController().activate("ajustes");
 		App.getAjustesController().setAnteriorController("menuPrincipal");
+		
+		final String JRXML_FILE = "/reports/factura.jrxml";
+		// Compila el informe
+		JasperReport report = JasperCompileManager.compileReport(getClass().getResourceAsStream(JRXML_FILE));
+
+		// Crea el mapa de parámetros para el informe (en este caso vacío)
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		// Generamos el informe (combinamos informe + datos)
+		JasperPrint jasperPrint = JasperFillManager.fillReport(report, parameters, new JRBeanArrayDataSource(App.getNivelesCompletados()
+				/*new JRBeanCollectionDataSource(App.getVolumenMusic()*/));
+
+		// Visualiza el informe generado
+		JasperViewer.viewReport(jasperPrint);
 	}
 
 	/**
 	 * Se inicia el juego y se ejecuta la vista y el controlador de Game
+	 * 
 	 * @param event
 	 */
 	@FXML
@@ -52,6 +79,7 @@ public class MenuPrincipalController implements Initializable, Controller {
 
 	/**
 	 * Salir del juego
+	 * 
 	 * @param event
 	 */
 	@FXML
